aec5329eb30c50533b19f5492913209d000e3561
zoolotoui <chag.med.amine@gmail.com>
2024-11-15 11:02:41 +0100

    CP-7 stat: creating competition

--- demo/src/main/java/com/example/demo/model/Competition.java
+++ demo/src/main/java/com/example/demo/model/Competition.java
@@ -1,11 +1,13 @@
 package com.example.demo.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Document
@@ -15,15 +17,17 @@ public class Competition {
 
     private String competitionName;
     private String cordGPS;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startDate;
     private double distance;
-
-    private Competition(String competitionName, String cordGPS, Date startDate, double distance) {
+    private List<String> pigeonRingNumbers;
+    private Competition(String competitionName, String cordGPS, Date startDate, double distance,List<String> pigeonRingNumbers) {
 
         this.competitionName = competitionName;
         this.cordGPS = cordGPS;
         this.startDate = startDate;
         this.distance = distance;
+        this.pigeonRingNumbers = pigeonRingNumbers;
 
     }
 
@@ -47,5 +51,9 @@ public class Competition {
 
     public void setDistance(double distance) {
         this.distance = distance;
+    }
+
+    public void setPigeonRingNumbers(List<String> pigeonRingNumbers) {
+        this.pigeonRingNumbers = pigeonRingNumbers;
     }
 }
