fd1787d5fab5920f0e94a4a7a35128e5ed892b05
zoolotoui <chag.med.amine@gmail.com>
2024-12-02 12:28:30 +0100

    stat: first insertion gone well

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
